af24e30cd0103962e0192edbaebda0a2dd773e72
Jesie Oldenburg <oldesdev@outlook.com>
2020-01-16 13:50:10 -0600

    started work on new file class_props.py; completed part one of stocks.py

--- exercises/stocks.py
+++ exercises/stocks.py
@@ -22,4 +22,37 @@ create_purchase_list('V', 300, '13-MAY-2007', 43.92)
 create_purchase_list('SDC', 200, '21-OCT-2006', 100.00)
 create_purchase_list('PEP', 150, '11-FEB-2012', 119.00)
 
-print(purchase_list)
+# print('\nPurchase list ::', purchase_list)
+
+"""
+Create a purchase history report that computes the full purchase price (shares times dollars) for each block of stock and uses the stockDict to look up the full company name. This is the basic relational database join algorithm between two tables.
+
+Example output for one block: I purchased General Electric stock for $4800
+"""
+
+def generate_single_stock_report():
+    # This works because we are only looping through the values. For the desired value in the range of values, do [...]
+    
+    for stocks_tuple in purchase_list:
+        # print(stocks_tuple)
+        num_of_shares = stocks_tuple[1]
+        share_price = stocks_tuple[3]
+        total_cost = num_of_shares * share_price
+        total_cost = str(total_cost)
+        # ticker = stocks_tuple[0]
+        for ticker in stocks_dict:
+            # print(stocks_dict[ticker])
+            full_company_name = stocks_dict[ticker]
+            full_company_name = str(full_company_name)
+            x = ticker in stocks_tuple
+            # print(x)
+            if x:
+                # print(full_company_name)
+                print(f'I purchased {full_company_name} stock for {total_cost}')
+                pass
+                
+            # print(x)
+generate_single_stock_report()
+    
+    
+    
