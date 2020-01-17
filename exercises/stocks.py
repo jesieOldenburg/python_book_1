a8d01218961e4b40fee648120d1072e28351e9df
Jesie Oldenburg <oldesdev@outlook.com>
2020-01-16 18:44:02 -0600

    continuing to work on stocks.py; Adding function to combine the stocks tuples withe the stocks dictionary and print out a summary of stock items

--- exercises/stocks.py
+++ exercises/stocks.py
@@ -16,11 +16,13 @@ def create_purchase_list(ticker, share, date, price):
     purch_list_item = tuple((ticker, share, date, price))
     purchase_list.append(purch_list_item)
     
-create_purchase_list('TGT', 100, '01-JAN-2020', 117.00)
-create_purchase_list('SQ', 100, '03-AUG-2003', 70.47)
-create_purchase_list('V', 300, '13-MAY-2007', 43.92)
-create_purchase_list('SDC', 200, '21-OCT-2006', 100.00)
-create_purchase_list('PEP', 150, '11-FEB-2012', 119.00)
+create_purchase_list('TGT', 100, '01-JAN-2020', 117)
+create_purchase_list('TGT', 100, '01-MAR-2001', 98)
+create_purchase_list('SQ', 100, '03-AUG-2003', 70)
+create_purchase_list('V', 300, '13-MAY-2007', 43)
+create_purchase_list('SDC', 200, '21-OCT-2006', 100)
+create_purchase_list('PEP', 150, '11-FEB-2012', 119)
+create_purchase_list('PEP', 100, '22-SEP-2015', 74)
 
 # print('\nPurchase list ::', purchase_list)
 
@@ -34,7 +36,6 @@ def generate_single_stock_report():
     # This works because we are only looping through the values. For the desired value in the range of values, do [...]
     
     for stocks_tuple in purchase_list:
-        # print(stocks_tuple)
         num_of_shares = stocks_tuple[1]
         share_price = stocks_tuple[3]
         total_cost = num_of_shares * share_price
@@ -47,12 +48,38 @@ def generate_single_stock_report():
             x = ticker in stocks_tuple
             # print(x)
             if x:
-                # print(full_company_name)
-                print(f'I purchased {full_company_name} stock for {total_cost}')
                 pass
+                # print(full_company_name)
+                # print(f'I purchased {full_company_name} stock for ${total_cost}')
                 
             # print(x)
 generate_single_stock_report()
     
-    
-    
+"""
+    Create a second purchase summary that which accumulates total investment by ticker symbol. In the above sample data, there are two blocks of GE. These can easily be combined by creating a dict where the key is the ticker and the value is the list of blocks purchased. The program makes one pass through the data to create the dict. A pass through the dict can then create a report showing each ticker symbol and all blocks of stock.
+
+    Example output:
+
+    ------ GE ------
+    100 shares at 48 dollars each on 01-jul-1998
+    200 shares at 56 dollars each on 10-sep-2001
+
+    Total value of stock in portfolio: $16000
+"""
+
+def combine_reports_to_dict():
+    combined_stocks = dict()
+
+    for k in purchase_list:
+        # print(purchase_list[:], '\n')
+        key = k[0]
+        vals = k[1:]
+        # print('Keys =>', key)
+        # print('Vals =>', vals, '\n')
+        if key not in combined_stocks:
+            combined_stocks[f'{key}'] = vals
+        else:
+            combined_stocks[f'{key}'] = purchase_list[:]
+            pass
+        print('\n', combined_stocks)
+combine_reports_to_dict()
