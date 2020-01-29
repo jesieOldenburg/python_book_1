9ebeae3ca3a13b9ce33c1a7c2c5cf787876a0d5f
Jesie Oldenburg <oldesdev@outlook.com>
2020-01-29 00:40:42 -0600

    added colors to rose class;  moved add flowers to arrangement

--- exercises/jakes_flowers/index.py
+++ exercises/jakes_flowers/index.py
@@ -5,5 +5,12 @@ from arrangements import Mothers_day_arrangement, Valentines_day_arrangement, Ar
 
 rose = Rose()
 bouquet_one = Mothers_day_arrangement()
-bouquet_one.add_flower(rose)
 bouquet_one.print_flowers()
+
+vday_bouquet = Valentines_day_arrangement()
+vday_bouquet.add_flower(rose)
+
+print(rose.available_colors)
+for color in rose.available_colors:
+    diff_color_rose = Rose(color)
+    print(diff_color_rose)
