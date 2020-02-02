a215c021e9e110d3ac8c67ff31e0b6881f908129
Jesie Oldenburg <kitglo46@gmail.com>
2020-02-01 18:04:11 -0600

    Set up initial classes for all types

---
+++ tracking_student-excercises/student.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, first_name, last_name, slack_handle, cohort_num):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.slack_handle = slack_handle
+        self.cohort_num = cohort_num
+        self.current_exercises = list()
+
+    def show_homework(self):
+        print(self.current_exercises[0])
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} is in Cohort {self.cohort_num}'
