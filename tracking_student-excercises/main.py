063c923b19fc9dd5122c3bcd90cf4fca4f80315f
Jesie Oldenburg <kitglo46@gmail.com>
2020-02-03 15:57:56 -0600

    working on for loops

--- tracking_student-excercises/main.py
+++ tracking_student-excercises/main.py
@@ -3,6 +3,9 @@ from exercise import Exercise
 from cohort import Cohort
 from instructor import Instructor
 
+
+
+
 # Create new instances of Cohorts
 day_cohort_36 = Cohort('Day Cohort 36')
 day_cohort_45 = Cohort('Day Cohort 45')
@@ -12,7 +15,6 @@ day_cohort_92 = Cohort('Day Cohort 92')
 jisie = Instructor('Jisie', 'David', 'jdavid', '36', 'Awesomeness')
 joe = Instructor('Joe', 'Shepard', 'JoeShep', '36', 'Cracking jokes')
 jenna = Instructor('Jenna', 'Sollis', 'Jsollis', '36', 'Eating my gum')
-brenda = Instructor('Brenda', 'Long', 'bjlong', '37', 'creativity')
 
 # * Assign the instructors to the cohorts
 day_cohort_36.add_instructor(jisie)
@@ -37,11 +39,23 @@ reverse_string = Exercise('Reverse String', 'JavaScript')
 party_time = Exercise('Party Time', 'Python')
 coins_to_cash = Exercise('Coins to Cash', 'Python')
 
+# TODO: Have each instructor assign 2 exercises to each of the students.
 # Assign exercises to students
 jisie.assign_exercise(jesie, fizzbuzz)
 jisie.assign_exercise(jesie, reverse_string)
-jisie.assign_exercise(jesie, party_time)
-jisie.assign_exercise(jesie, coins_to_cash)
+jenna.assign_exercise(jesie, party_time)
+jenna.assign_exercise(jesie, coins_to_cash)
+joe.assign_exercise(jesie, fizzbuzz)
+joe.assign_exercise(jesie, coins_to_cash)
+
+jisie.assign_exercise(guy, party_time)
+jisie.assign_exercise(guy, coins_to_cash)
+jenna.assign_exercise(guy, party_time)
+jenna.assign_exercise(guy, reverse_string)
+joe.assign_exercise(guy, fizzbuzz)
+joe.assign_exercise(guy, coins_to_cash)
+
+
 
 # Print the students homework to the console
 # jesie.show_homework()
@@ -59,9 +73,14 @@ exercises.append(reverse_string)
 exercises.append(party_time)
 exercises.append(coins_to_cash)
 
+# ? What is the condition I need to check here?
+# I am iterating through the 
 for s in students:
+    # ! s == to an instance of a student
     student_name  = s.first_name + ' ' + s.last_name
-    print('s =>', student_name)
+    print(s.current_exercises)
+    # print('s =>', student_name)
     for x in exercises:
-        print(f'{student_name} is working on the {x.exercise_name} exercise')
+        print(x.exercise_name)
+        # print(f'{student_name} is working on the {x.exercise_name} exercise')
         pass
